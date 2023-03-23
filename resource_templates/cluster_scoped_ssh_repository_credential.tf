f216a7bb4de63bcc7c1f6f7e7f533585024d48c7
Will Arroyo <warroyo7199008@gmail.com>
2023-03-23 15:47:02 -0600

    adding resource templates
    Signed-off-by: Will Arroyo <warroyo7199008@gmail.com>

---
+++ resource_templates/cluster_scoped_ssh_repository_credential.tf
@@ -0,0 +1,28 @@
+# Create Tanzu Mission Control repositorycredential entry
+resource "tanzu-mission-control_repository_credential" "test_credential" {
+ name = "resposiroy-credential-name"
+
+ scope {
+   cluster {
+      cluster_name            = "testcluster" # Required
+      provisioner_name        = "attached"     # Default: attached
+      management_cluster_name = "attached"     # Default: attached
+   }
+ }
+
+ spec {
+   // this block to have any fields specific to cluster group level resource (ex- fanout config) - custom schema validations will be added for these attributes to restrict them to a particular scope
+  
+   source_secret_type = "SSH"
+
+   data = {
+     ssh_key = "somesshkey"
+     known_hosts = "knowshosts"
+   }
+ }
+
+ // Since this is not asynchronously updated it might not be needed, the terraform resource’s state might be able to convey if the credential has been created or not.
+ status {
+   credential_phase = "CREATED"
+ }
+}
