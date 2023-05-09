c7b596d82aed6653bd870c07541b6c33fb39c366
Shreyas Sreenivas <sshreyas@vmware.com>
2023-05-09 18:05:31 +0530

    Implement auth context to work with TMC self-managed instance
    Signed-off-by: Shreyas Sreenivas <sshreyas@vmware.com>

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,4 @@
+# Provider configuration for TMC SaaS
 provider "tanzu-mission-control" {
   endpoint            = var.endpoint            # optionally use TMC_ENDPOINT env var
   vmw_cloud_api_token = var.vmw_cloud_api_token # optionally use VMW_CLOUD_API_TOKEN env var
@@ -5,4 +6,15 @@ provider "tanzu-mission-control" {
   # if you are using dev or different csp endpoint, change the default value below
   # for production environments the vmw_cloud_endpoint is console.cloud.vmware.com
   # vmw_cloud_endpoint = "console.cloud.vmware.com" or optionally use VMW_CLOUD_ENDPOINT env var
+}
+
+# Provider configuration for TMC Self-Managed
+provider "tanzu-mission-control" {
+  endpoint = "example.local-dev.tmc.com"
+
+  self_managed {
+    oidc_issuer = "pinniped-supervisor.example.local-dev.tmc.com"
+    username = "testuser01@tmcselfmanaged.com"
+    password = "dummy_password"
+  }
 }
