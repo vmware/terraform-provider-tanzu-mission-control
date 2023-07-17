1fdb8fad16aeec13a2766da8d4cca3d3b210acc9
Ramya Bangera <bangerar@vmware.com>
2023-07-17 17:00:23 +0530

    Add support to read TMC self managed configuration details from env variables
    Signed-off-by: Ramya Bangera <bangerar@vmware.com>

--- examples/provider/variables.tf
+++ examples/provider/variables.tf
@@ -7,3 +7,12 @@ variable "vmw_cloud_api_token" {
 variable "vmw_cloud_endpoint" {
   default = "console.cloud.vmware.com"
 }
+
+variable "oidc_issuer" {
+}
+
+variable "username" {
+}
+
+variable "password" {
+}
