406ef6079f09c4891d2da3d8048f0cea4a99546c
Sammota Mwakalobo <smwakalobo@vmware.com>
2023-07-26 12:00:18 -0600

    Add generated markdown docs for AKS
    Signed-off-by: Sammota Mwakalobo <smwakalobo@vmware.com>

---
+++ examples/data-sources/akscluster/cluster.tf
@@ -0,0 +1,7 @@
+// Read Tanzu Mission Control Azure AKS cluster : fetch cluster details
+data "tanzu-mission-control_akscluster" "tf_aks_cluster" {
+  credential_name = "test-azure-credential" // Required
+  subscription    = "test-azure-subscription"    // Required
+  resource_group  = "test-azure-resource-group"  // Required
+  name            = "test-aks-cluster>"    // Required
+}
