33865ffdff26e6163fdfa19b1956965938e39390
GilTS <gil@terasky.com>
2023-10-03 19:11:34 +0300

    Fixed schema descriptions and keys access level
    Signed-off-by: GilTS <gil@terasky.com>

--- internal/resources/cluster/backupschedule/datasource_schema.go
+++ internal/resources/cluster/backupschedule/datasource_schema.go
@@ -11,61 +11,85 @@ import (
 
 const (
 	// Root Directive Keys.
-	sortByKey            = "sort_by"
-	queryKey             = "query"
-	includeTotalCountKey = "include_total_count"
-	schedulesKey         = "schedules"
-	totalCountKey        = "total_count"
+	SortByKey            = "sort_by"
+	QueryKey             = "query"
+	IncludeTotalCountKey = "include_total_count"
+	SchedulesKey         = "schedules"
+	TotalCountKey        = "total_count"
 )
 
 var backupScheduleDataSourceSchema = map[string]*schema.Schema{
-	scopeKey:             searchScopeSchema,
-	sortByKey:            sortBySchema,
-	queryKey:             querySchema,
-	includeTotalCountKey: includeTotalSchema,
-	schedulesKey:         schedulesSchema,
-	totalCountKey:        totalCountSchema,
+	ScopeKey:             searchScopeSchema,
+	SortByKey:            sortBySchema,
+	QueryKey:             querySchema,
+	IncludeTotalCountKey: includeTotalSchema,
+	SchedulesKey:         schedulesSchema,
+	TotalCountKey:        totalCountSchema,
 }
 
 var searchScopeSchema = &schema.Schema{
-	Type:     schema.TypeList,
-	MaxItems: 1,
-	Required: true,
+	Type:        schema.TypeList,
+	Description: "Search scope block",
+	MaxItems:    1,
+	Required:    true,
 	Elem: &schema.Resource{
 		Schema: map[string]*schema.Schema{
-			clusterNameKey:           clusterNameSchema,
-			managementClusterNameKey: managementClusterNameSchema,
-			provisionerNameKey:       provisionerNameSchema,
-			nameKey:                  nameSchema,
+			ClusterNameKey:           clusterNameSchema,
+			ManagementClusterNameKey: managementClusterNameDSSchema,
+			ProvisionerNameKey:       provisionerNameDSSchema,
+			NameKey:                  nameDSSchema,
 		},
 	},
 }
 
+var nameDSSchema = &schema.Schema{
+	Type:        schema.TypeString,
+	Description: "The name of the backup schedule",
+	Optional:    true,
+}
+
+var managementClusterNameDSSchema = &schema.Schema{
+	Type:        schema.TypeString,
+	Description: "Management cluster name",
+	Optional:    true,
+}
+
+var provisionerNameDSSchema = &schema.Schema{
+	Type:        schema.TypeString,
+	Description: "Cluster provisioner name",
+	Optional:    true,
+}
+
 var sortBySchema = &schema.Schema{
-	Type:     schema.TypeString,
-	Optional: true,
+	Type:        schema.TypeString,
+	Description: "Sort backups by field.",
+	Optional:    true,
 }
 
 var querySchema = &schema.Schema{
-	Type:     schema.TypeString,
-	Optional: true,
+	Type:        schema.TypeString,
+	Description: "Define a query for listing backups",
+	Optional:    true,
 }
 
 var includeTotalSchema = &schema.Schema{
-	Type:     schema.TypeBool,
-	Optional: true,
-	Default:  true,
+	Type:        schema.TypeBool,
+	Description: "Whether to include total count of backups.\n(Default: True)",
+	Optional:    true,
+	Default:     true,
 }
 
 var schedulesSchema = &schema.Schema{
-	Type:     schema.TypeList,
-	Computed: true,
+	Type:        schema.TypeList,
+	Description: "A list of schedules returned",
+	Computed:    true,
 	Elem: &schema.Resource{
 		Schema: backupScheduleResourceSchema,
 	},
 }
 
 var totalCountSchema = &schema.Schema{
-	Type:     schema.TypeString,
-	Computed: true,
+	Type:        schema.TypeString,
+	Description: "Total count of schedules returned",
+	Computed:    true,
 }
