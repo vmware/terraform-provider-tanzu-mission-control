261537da902f35048eb636e1e6f002cdf3fc10a6
vshubhang <vshubhang@vmware.com>
2023-10-05 11:26:48 +0530

    Add models and clients for CG secret
    Signed-off-by: vshubhang <vshubhang@vmware.com>

---
+++ internal/models/kubernetessecret/clustergroup/secretexport/fullname.go
@@ -0,0 +1,47 @@
+/*
+Copyright © 2023 VMware, Inc. All Rights Reserved.
+SPDX-License-Identifier: MPL-2.0
+*/
+
+package clustergroupsecretexport
+
+import "github.com/go-openapi/swag"
+
+// VmwareTanzuManageV1alpha1ClustergroupNamespaceSecretexportFullName Full name of the Secret Export.
+//
+// swagger:model vmware.tanzu.manage.v1alpha1.clustergroup.namespace.secretexport.FullName
+type VmwareTanzuManageV1alpha1ClustergroupNamespaceSecretexportFullName struct {
+
+	// Name of Cluster Group.
+	ClusterGroupName string `json:"clusterGroupName,omitempty"`
+
+	// Name of the Secret Export (expected to share the same name of the secret).
+	Name string `json:"name,omitempty"`
+
+	// Name of Namespace.
+	NamespaceName string `json:"namespaceName,omitempty"`
+
+	// ID of Organization.
+	OrgID string `json:"orgId,omitempty"`
+}
+
+// MarshalBinary interface implementation.
+func (m *VmwareTanzuManageV1alpha1ClustergroupNamespaceSecretexportFullName) MarshalBinary() ([]byte, error) {
+	if m == nil {
+		return nil, nil
+	}
+
+	return swag.WriteJSON(m)
+}
+
+// UnmarshalBinary interface implementation.
+func (m *VmwareTanzuManageV1alpha1ClustergroupNamespaceSecretexportFullName) UnmarshalBinary(b []byte) error {
+	var res VmwareTanzuManageV1alpha1ClustergroupNamespaceSecretexportFullName
+	if err := swag.ReadJSON(b, &res); err != nil {
+		return err
+	}
+
+	*m = res
+
+	return nil
+}
