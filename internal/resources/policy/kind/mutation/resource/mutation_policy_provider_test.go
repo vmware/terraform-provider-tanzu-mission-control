54b2e387913722fdd421ebdce5a2a6d3770e8fe8
Erki Koop <erki.k@terasky.com>
2023-10-15 14:17:04 +0300

    Add review changes
    Signed-off-by: Erki Koop <erki.k@terasky.com>

--- internal/resources/policy/kind/mutation/resource/mutation_policy_provider_test.go
+++ internal/resources/policy/kind/mutation/resource/mutation_policy_provider_test.go
@@ -6,8 +6,11 @@ SPDX-License-Identifier: MPL-2.0
 package mutationpolicyresource
 
 import (
+	"context"
+	"os"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/stretchr/testify/require"
 
@@ -17,7 +20,7 @@ import (
 	policykindmutation "github.com/vmware/terraform-provider-tanzu-mission-control/internal/resources/policy/kind/mutation"
 )
 
-func initMutationPolicyTestProvider(t *testing.T) *schema.Provider { // nolint:unused
+func initMutationPolicyTestProvider(t *testing.T) *schema.Provider {
 	testAccProvider := &schema.Provider{
 		Schema: authctx.ProviderAuthSchema(),
 		ResourcesMap: map[string]*schema.Resource{
@@ -25,11 +28,19 @@ func initMutationPolicyTestProvider(t *testing.T) *schema.Provider { // nolint:u
 			cluster.ResourceName:            cluster.ResourceTMCCluster(),
 			clustergroup.ResourceName:       clustergroup.ResourceClusterGroup(),
 		},
-		ConfigureContextFunc: authctx.ProviderConfigureContext,
+		ConfigureContextFunc: getConfigureContextFunc(),
 	}
 	if err := testAccProvider.InternalValidate(); err != nil {
 		require.NoError(t, err)
 	}
 
 	return testAccProvider
+}
+
+func getConfigureContextFunc() func(_ context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	if _, found := os.LookupEnv("ENABLE_POLICY_ENV_TEST"); !found {
+		return authctx.ProviderConfigureContextWithDefaultTransportForTesting
+	}
+
+	return authctx.ProviderConfigureContext
 }
--- internal/resources/policy/kind/mutation/resource/resource_mutation_policy_test.go
+++ internal/resources/policy/kind/mutation/resource/resource_mutation_policy_test.go
@@ -15,7 +15,6 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/pkg/errors"
 
@@ -25,26 +24,10 @@ import (
 	policyclustergroupmodel "github.com/vmware/terraform-provider-tanzu-mission-control/internal/models/policy/clustergroup"
 	policyorganizationmodel "github.com/vmware/terraform-provider-tanzu-mission-control/internal/models/policy/organization"
 	"github.com/vmware/terraform-provider-tanzu-mission-control/internal/resources/policy"
-	policykindmutation "github.com/vmware/terraform-provider-tanzu-mission-control/internal/resources/policy/kind/mutation"
 	policyoperations "github.com/vmware/terraform-provider-tanzu-mission-control/internal/resources/policy/operations"
 	"github.com/vmware/terraform-provider-tanzu-mission-control/internal/resources/policy/scope"
 	testhelper "github.com/vmware/terraform-provider-tanzu-mission-control/internal/resources/testing"
 )
-
-const (
-	mutationPolicyResource    = policykindmutation.ResourceName
-	mutationPolicyResourceVar = "test_mutation_policy"
-	mutationPolicyNamePrefix  = "tf-mp-test"
-)
-
-type testAcceptanceConfig struct {
-	Provider                   *schema.Provider
-	MutationPolicyResource     string
-	MutationPolicyResourceVar  string
-	MutationPolicyResourceName string
-	MutationPolicyName         string
-	ScopeHelperResources       *policy.ScopeHelperResources
-}
 
 func testGetDefaultAcceptanceConfig(t *testing.T) *testAcceptanceConfig {
 	return &testAcceptanceConfig{
@@ -60,6 +43,30 @@ func testGetDefaultAcceptanceConfig(t *testing.T) *testAcceptanceConfig {
 func TestAcceptanceForMutationPolicyResource(t *testing.T) {
 	testConfig := testGetDefaultAcceptanceConfig(t)
 
+	var found bool
+	if _, found = os.LookupEnv("ENABLE_POLICY_ENV_TEST"); !found {
+		os.Setenv("TF_ACC", "true")
+		os.Setenv("TMC_ENDPOINT", "play.abc.def.ghi.com")
+		os.Setenv("VMW_CLOUD_API_TOKEN", "dummy")
+		os.Setenv("VMW_CLOUD_ENDPOINT", "console.cloud.vmware.com")
+
+		testConfig.ScopeHelperResources.OrgID = "dummy_org"
+		testConfig.setupHTTPMocks(t)
+	} else {
+		requiredVars := []string{
+			"VMW_CLOUD_ENDPOINT",
+			"TMC_ENDPOINT",
+			"VMW_CLOUD_API_TOKEN",
+			"ORG_ID",
+		}
+
+		for _, name := range requiredVars {
+			if _, found := os.LookupEnv(name); !found {
+				t.Errorf("required environment variable '%s' missing", name)
+			}
+		}
+	}
+
 	t.Log("Start mutation policy resource acceptance tests!")
 
 	resource.Test(t, resource.TestCase{
@@ -68,8 +75,43 @@ func TestAcceptanceForMutationPolicyResource(t *testing.T) {
 		CheckDestroy:      nil,
 		Steps: []resource.TestStep{
 			{
-				Config: testConfig.getTestMutationPolicyResourceBasicConfigValue(scope.ClusterGroupScope, getPodSecurityResourceInput()),
-				Check:  testConfig.checkClusterGroupScopeMutationPolicyResourceAttributes(),
+				Config: testConfig.getTestMutationPolicyResourceBasicConfigValue(podSecurity, scope.ClusterGroupScope, getPodSecurityResourceInput()),
+				Check:  testConfig.checkClusterGroupScopeMutationPolicyResourceAttributes(podSecurity),
+			},
+			{
+				Config: testConfig.getTestMutationPolicyResourceBasicConfigValue(label, scope.ClusterGroupScope, getLabelResourceInput()),
+				Check:  testConfig.checkClusterGroupScopeMutationPolicyResourceAttributes(label),
+			},
+			{
+				Config: testConfig.getTestMutationPolicyResourceBasicConfigValue(annotation, scope.ClusterGroupScope, getAnnotationResourceInput()),
+				Check:  testConfig.checkClusterGroupScopeMutationPolicyResourceAttributes(annotation),
+			},
+			{
+				PreConfig: func() {
+					if testConfig.ScopeHelperResources.OrgID == "" {
+						t.Skip("ORG_ID env var is not set for organization scoped mutation policy acceptance test")
+					}
+				},
+				Config: testConfig.getTestMutationPolicyResourceBasicConfigValue(podSecurity, scope.OrganizationScope, getPodSecurityResourceInput()),
+				Check:  testConfig.checkOrganizationScopeMutationPolicyResourceAttributes(podSecurity),
+			},
+			{
+				PreConfig: func() {
+					if testConfig.ScopeHelperResources.OrgID == "" {
+						t.Skip("ORG_ID env var is not set for organization scoped mutation policy acceptance test")
+					}
+				},
+				Config: testConfig.getTestMutationPolicyResourceBasicConfigValue(label, scope.OrganizationScope, getLabelResourceInput()),
+				Check:  testConfig.checkOrganizationScopeMutationPolicyResourceAttributes(label),
+			},
+			{
+				PreConfig: func() {
+					if testConfig.ScopeHelperResources.OrgID == "" {
+						t.Skip("ORG_ID env var is not set for organization scoped mutation policy acceptance test")
+					}
+				},
+				Config: testConfig.getTestMutationPolicyResourceBasicConfigValue(annotation, scope.OrganizationScope, getAnnotationResourceInput()),
+				Check:  testConfig.checkOrganizationScopeMutationPolicyResourceAttributes(annotation),
 			},
 			{
 				PreConfig: func() {
@@ -77,21 +119,8 @@ func TestAcceptanceForMutationPolicyResource(t *testing.T) {
 						t.Skip("KUBECONFIG env var is not set for cluster group scoped mutation policy acceptance test")
 					}
 				},
-				Config: testConfig.getTestMutationPolicyResourceBasicConfigValue(scope.ClusterScope, getPodSecurityResourceInput()),
-				Check:  testConfig.checkClusterScopeMutationPolicyResourceAttributes(),
-			},
-			{
-				PreConfig: func() {
-					if testConfig.ScopeHelperResources.OrgID == "" {
-						t.Skip("ORG_ID env var is not set for organization scoped mutation policy acceptance test")
-					}
-				},
-				Config: testConfig.getTestMutationPolicyResourceBasicConfigValue(scope.OrganizationScope, getPodSecurityResourceInput()),
-				Check:  testConfig.checkOrganizationScopeMutationPolicyResourceAttributes(),
-			},
-			{
-				Config: testConfig.getTestMutationPolicyResourceBasicConfigValue(scope.ClusterGroupScope, getLabelResourceInput()),
-				Check:  testConfig.checkClusterGroupScopeMutationPolicyResourceAttributes(),
+				Config: testConfig.getTestMutationPolicyResourceBasicConfigValue(podSecurity, scope.ClusterScope, getPodSecurityResourceInput()),
+				Check:  testConfig.checkClusterScopeMutationPolicyResourceAttributes(podSecurity),
 			},
 			{
 				PreConfig: func() {
@@ -99,22 +128,8 @@ func TestAcceptanceForMutationPolicyResource(t *testing.T) {
 						t.Skip("KUBECONFIG env var is not set for cluster scoped mutation policy acceptance test")
 					}
 				},
-				Config: testConfig.getTestMutationPolicyResourceBasicConfigValue(scope.ClusterScope, getLabelResourceInput()),
-				Check:  testConfig.checkClusterScopeMutationPolicyResourceAttributes(),
-			},
-			{
-
-				PreConfig: func() {
-					if testConfig.ScopeHelperResources.OrgID == "" {
-						t.Skip("ORG_ID env var is not set for organization scoped mutation policy acceptance test")
-					}
-				},
-				Config: testConfig.getTestMutationPolicyResourceBasicConfigValue(scope.OrganizationScope, getLabelResourceInput()),
-				Check:  testConfig.checkOrganizationScopeMutationPolicyResourceAttributes(),
-			},
-			{
-				Config: testConfig.getTestMutationPolicyResourceBasicConfigValue(scope.ClusterGroupScope, getAnnotationResourceInput()),
-				Check:  testConfig.checkClusterGroupScopeMutationPolicyResourceAttributes(),
+				Config: testConfig.getTestMutationPolicyResourceBasicConfigValue(label, scope.ClusterScope, getLabelResourceInput()),
+				Check:  testConfig.checkClusterScopeMutationPolicyResourceAttributes(label),
 			},
 			{
 				PreConfig: func() {
@@ -122,17 +137,8 @@ func TestAcceptanceForMutationPolicyResource(t *testing.T) {
 						t.Skip("KUBECONFIG env var is not set for cluster scoped mutation policy acceptance test")
 					}
 				},
-				Config: testConfig.getTestMutationPolicyResourceBasicConfigValue(scope.ClusterScope, getAnnotationResourceInput()),
-				Check:  testConfig.checkClusterScopeMutationPolicyResourceAttributes(),
-			},
-			{
-				PreConfig: func() {
-					if testConfig.ScopeHelperResources.OrgID == "" {
-						t.Skip("ORG_ID env var is not set for organization scoped mutation policy acceptance test")
-					}
-				},
-				Config: testConfig.getTestMutationPolicyResourceBasicConfigValue(scope.OrganizationScope, getAnnotationResourceInput()),
-				Check:  testConfig.checkOrganizationScopeMutationPolicyResourceAttributes(),
+				Config: testConfig.getTestMutationPolicyResourceBasicConfigValue(annotation, scope.ClusterScope, getAnnotationResourceInput()),
+				Check:  testConfig.checkClusterScopeMutationPolicyResourceAttributes(annotation),
 			},
 		},
 	},
@@ -141,7 +147,7 @@ func TestAcceptanceForMutationPolicyResource(t *testing.T) {
 	t.Log("Mutation policy resource acceptance test complete!")
 }
 
-func (testConfig *testAcceptanceConfig) getTestMutationPolicyResourceBasicConfigValue(scope scope.Scope, inputBlock string) string {
+func (testConfig *testAcceptanceConfig) getTestMutationPolicyResourceBasicConfigValue(recipe string, scope scope.Scope, inputBlock string) string {
 	helperBlock, scopeBlock := testConfig.ScopeHelperResources.GetTestPolicyResourceHelperAndScope(scope, policyoperations.ScopeMap[testConfig.MutationPolicyResource], false)
 
 	return fmt.Sprintf(`
@@ -172,13 +178,13 @@ func (testConfig *testAcceptanceConfig) getTestMutationPolicyResourceBasicConfig
 	   }
 	 }
 	}
-	`, helperBlock, testConfig.MutationPolicyResource, testConfig.MutationPolicyResourceVar, testConfig.MutationPolicyName, scopeBlock, inputBlock)
+	`, helperBlock, testConfig.MutationPolicyResource, testConfig.MutationPolicyResourceVar, testConfig.MutationPolicyName+recipe, scopeBlock, inputBlock)
 }
 
-func (testConfig *testAcceptanceConfig) checkClusterScopeMutationPolicyResourceAttributes() resource.TestCheckFunc {
+func (testConfig *testAcceptanceConfig) checkClusterScopeMutationPolicyResourceAttributes(recipe string) resource.TestCheckFunc {
 	var check = []resource.TestCheckFunc{
-		testConfig.verifyClusterScopeMutationPolicyResourceCreation(),
-		resource.TestCheckResourceAttr(testConfig.MutationPolicyResourceName, "name", testConfig.MutationPolicyName),
+		testConfig.verifyClusterScopeMutationPolicyResourceCreation(recipe),
+		resource.TestCheckResourceAttr(testConfig.MutationPolicyResourceName, "name", testConfig.MutationPolicyName+recipe),
 	}
 
 	check = append(check, resource.TestCheckResourceAttr(testConfig.MutationPolicyResourceName, "scope.0.cluster.0.name", testConfig.ScopeHelperResources.Cluster.Name))
@@ -187,10 +193,10 @@ func (testConfig *testAcceptanceConfig) checkClusterScopeMutationPolicyResourceA
 	return resource.ComposeTestCheckFunc(check...)
 }
 
-func (testConfig *testAcceptanceConfig) checkClusterGroupScopeMutationPolicyResourceAttributes() resource.TestCheckFunc {
+func (testConfig *testAcceptanceConfig) checkClusterGroupScopeMutationPolicyResourceAttributes(recipe string) resource.TestCheckFunc {
 	var check = []resource.TestCheckFunc{
-		testConfig.verifyClusterGroupScopeMutationPolicyResourceCreation(),
-		resource.TestCheckResourceAttr(testConfig.MutationPolicyResourceName, "name", testConfig.MutationPolicyName),
+		testConfig.verifyClusterGroupScopeMutationPolicyResourceCreation(recipe),
+		resource.TestCheckResourceAttr(testConfig.MutationPolicyResourceName, "name", testConfig.MutationPolicyName+recipe),
 	}
 
 	check = append(check, resource.TestCheckResourceAttr(testConfig.MutationPolicyResourceName, "scope.0.cluster_group.0.cluster_group", testConfig.ScopeHelperResources.ClusterGroup.Name))
@@ -199,10 +205,10 @@ func (testConfig *testAcceptanceConfig) checkClusterGroupScopeMutationPolicyReso
 	return resource.ComposeTestCheckFunc(check...)
 }
 
-func (testConfig *testAcceptanceConfig) checkOrganizationScopeMutationPolicyResourceAttributes() resource.TestCheckFunc {
+func (testConfig *testAcceptanceConfig) checkOrganizationScopeMutationPolicyResourceAttributes(recipe string) resource.TestCheckFunc {
 	var check = []resource.TestCheckFunc{
-		testConfig.verifyOrganizationScopeMutationPolicyResourceCreation(),
-		resource.TestCheckResourceAttr(testConfig.MutationPolicyResourceName, "name", testConfig.MutationPolicyName),
+		testConfig.verifyOrganizationScopeMutationPolicyResourceCreation(recipe),
+		resource.TestCheckResourceAttr(testConfig.MutationPolicyResourceName, "name", testConfig.MutationPolicyName+recipe),
 	}
 
 	check = append(check, resource.TestCheckResourceAttr(testConfig.MutationPolicyResourceName, "scope.0.organization.0.organization", testConfig.ScopeHelperResources.OrgID))
@@ -211,7 +217,7 @@ func (testConfig *testAcceptanceConfig) checkOrganizationScopeMutationPolicyReso
 	return resource.ComposeTestCheckFunc(check...)
 }
 
-func (testConfig *testAcceptanceConfig) verifyClusterScopeMutationPolicyResourceCreation() resource.TestCheckFunc {
+func (testConfig *testAcceptanceConfig) verifyClusterScopeMutationPolicyResourceCreation(recipe string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		config, err := testConfig.getContext(s)
 		if err != nil {
@@ -221,7 +227,7 @@ func (testConfig *testAcceptanceConfig) verifyClusterScopeMutationPolicyResource
 		fn := &policyclustermodel.VmwareTanzuManageV1alpha1ClusterPolicyFullName{
 			ClusterName:           testConfig.ScopeHelperResources.Cluster.Name,
 			ManagementClusterName: scope.AttachedValue,
-			Name:                  testConfig.MutationPolicyName,
+			Name:                  testConfig.MutationPolicyName + recipe,
 			ProvisionerName:       scope.AttachedValue,
 		}
 
@@ -238,7 +244,7 @@ func (testConfig *testAcceptanceConfig) verifyClusterScopeMutationPolicyResource
 	}
 }
 
-func (testConfig *testAcceptanceConfig) verifyClusterGroupScopeMutationPolicyResourceCreation() resource.TestCheckFunc {
+func (testConfig *testAcceptanceConfig) verifyClusterGroupScopeMutationPolicyResourceCreation(recipe string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		config, err := testConfig.getContext(s)
 		if err != nil {
@@ -247,7 +253,7 @@ func (testConfig *testAcceptanceConfig) verifyClusterGroupScopeMutationPolicyRes
 
 		fn := &policyclustergroupmodel.VmwareTanzuManageV1alpha1ClustergroupPolicyFullName{
 			ClusterGroupName: testConfig.ScopeHelperResources.ClusterGroup.Name,
-			Name:             testConfig.MutationPolicyName,
+			Name:             testConfig.MutationPolicyName + recipe,
 		}
 
 		resp, err := config.TMCConnection.ClusterGroupPolicyResourceService.ManageV1alpha1ClustergroupPolicyResourceServiceGet(fn)
@@ -263,7 +269,7 @@ func (testConfig *testAcceptanceConfig) verifyClusterGroupScopeMutationPolicyRes
 	}
 }
 
-func (testConfig *testAcceptanceConfig) verifyOrganizationScopeMutationPolicyResourceCreation() resource.TestCheckFunc {
+func (testConfig *testAcceptanceConfig) verifyOrganizationScopeMutationPolicyResourceCreation(recipe string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		config, err := testConfig.getContext(s)
 		if err != nil {
@@ -272,7 +278,7 @@ func (testConfig *testAcceptanceConfig) verifyOrganizationScopeMutationPolicyRes
 
 		fn := &policyorganizationmodel.VmwareTanzuManageV1alpha1OrganizationPolicyFullName{
 			OrgID: testConfig.ScopeHelperResources.OrgID,
-			Name:  testConfig.MutationPolicyName,
+			Name:  testConfig.MutationPolicyName + recipe,
 		}
 
 		resp, err := config.TMCConnection.OrganizationPolicyResourceService.ManageV1alpha1OrganizationPolicyResourceServiceGet(fn)
@@ -304,7 +310,9 @@ func (testConfig *testAcceptanceConfig) getContext(s *terraform.State) (*authctx
 		TLSConfig:        &proxy.TLSConfig{},
 	}
 
-	if err := config.Setup(); err != nil {
+	err := getSetupConfig(config)
+
+	if err != nil {
 		return nil, errors.Wrap(err, "unable to set the context")
 	}
 
@@ -336,7 +344,7 @@ func getAnnotationResourceInput() string {
 func getLabelResourceInput() string {
 	return `
     input {
-      annotation {
+      label {
         target_kubernetes_resources {
           api_groups = [
             "apps"
@@ -346,7 +354,7 @@ func getLabelResourceInput() string {
           ]
         }
         scope = "Cluster"
-        annotation {
+        label {
           key   = "test"
           value = "optional"
         }
