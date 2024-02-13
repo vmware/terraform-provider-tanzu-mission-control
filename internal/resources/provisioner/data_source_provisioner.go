543c51f8c2e73280f82a20ee0ee729d88f926a39
Shobha M <mshobha@vmware.com>
2024-02-13 16:22:36 +0530

    Fix Provisioner List Data read
    Signed-off-by: Shobha M <mshobha@vmware.com>

--- internal/resources/provisioner/data_source_provisioner.go
+++ internal/resources/provisioner/data_source_provisioner.go
@@ -7,6 +7,7 @@ package provisioner
 
 import (
 	"context"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -15,6 +16,8 @@ import (
 	"github.com/vmware/terraform-provider-tanzu-mission-control/internal/authctx"
 	clienterrors "github.com/vmware/terraform-provider-tanzu-mission-control/internal/client/errors"
 	"github.com/vmware/terraform-provider-tanzu-mission-control/internal/helper"
+	objectmetamodel "github.com/vmware/terraform-provider-tanzu-mission-control/internal/models/objectmeta"
+	provisioner "github.com/vmware/terraform-provider-tanzu-mission-control/internal/models/provisioner"
 	"github.com/vmware/terraform-provider-tanzu-mission-control/internal/resources/common"
 )
 
@@ -28,35 +31,48 @@ func DataSourceProvisioner() *schema.Resource {
 }
 
 var provisionerListSchema = map[string]*schema.Schema{
-	nameKey: {
-		Type:        schema.TypeString,
-		Description: "Name of the provisioner",
+	provisionerKey: {
+		Type:        schema.TypeList,
+		Description: "Provisioners info",
 		Optional:    true,
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				nameKey: {
+					Type:        schema.TypeString,
+					Description: "Name of the provisioner",
+					Optional:    true,
+				},
+				managementClusterNameKey: {
+					Type:        schema.TypeString,
+					Description: "Name of the management cluster",
+					Required:    true,
+					ForceNew:    true,
+				},
+				orgIDKey: {
+					Type:        schema.TypeString,
+					Description: "ID of the organization",
+					Optional:    true,
+				},
+				common.MetaKey: common.Meta,
+			},
+		},
 	},
-	managementClusterNameKey: {
-		Type:        schema.TypeString,
-		Description: "Name of the management cluster",
-		Required:    true,
-		ForceNew:    true,
-	},
-	orgIDKey: {
-		Type:        schema.TypeString,
-		Description: "ID of the organization",
-		Optional:    true,
-	},
-	common.MetaKey: common.Meta,
 }
 
 func dataSourceProvisionerRead(ctx context.Context, d *schema.ResourceData, m interface{}) (diags diag.Diagnostics) {
 	config := m.(authctx.TanzuContext)
 
-	model, err := tfModelConverter.ConvertTFSchemaToAPIModel(d, []string{nameKey, managementClusterNameKey})
-	if err != nil {
+	id := make([]string, 0)
+
+	var resp *provisioner.VmwareTanzuManageV1alpha1ManagementclusterProvisionerListprovisionersResponse
+
+	model, err := tfModelDataConverter.ConvertTFSchemaToAPIModel(d, []string{provisionerKey, nameKey, managementClusterNameKey})
+	if err != nil || model == nil {
 		return diag.FromErr(errors.Wrapf(err, "Couldn't read Tanzu Mission Control provisioner configurations."))
 	}
 
-	if model.FullName.Name == "" {
-		resp, err := config.TMCConnection.ProvisionerResourceService.ProvisionerResourceServiceList(model.FullName)
+	if model.Provisioners[0].FullName.Name == "" {
+		resp, err = config.TMCConnection.ProvisionerResourceService.ProvisionerResourceServiceList(model.Provisioners[0].FullName)
 		if err != nil {
 			if clienterrors.IsNotFoundError(err) && !helper.IsDataRead(ctx) {
 				_ = schema.RemoveFromState(d, m)
@@ -64,17 +80,9 @@ func dataSourceProvisionerRead(ctx context.Context, d *schema.ResourceData, m in
 			}
 
 			return
-		}
-
-		for i := range resp.Provisioners {
-			d.SetId(resp.Provisioners[i].Meta.UID)
-
-			if err := d.Set(common.MetaKey, common.FlattenMeta(resp.Provisioners[i].Meta)); err != nil {
-				return diag.FromErr(err)
-			}
 		}
 	} else {
-		resp, err := config.TMCConnection.ProvisionerResourceService.ProvisionerResourceServiceGet(model.FullName)
+		getResp, err := config.TMCConnection.ProvisionerResourceService.ProvisionerResourceServiceGet(model.Provisioners[0].FullName)
 		if err != nil {
 			if clienterrors.IsNotFoundError(err) && !helper.IsDataRead(ctx) {
 				_ = schema.RemoveFromState(d, m)
@@ -83,12 +91,38 @@ func dataSourceProvisionerRead(ctx context.Context, d *schema.ResourceData, m in
 			return
 		}
 
-		d.SetId(resp.Provisioner.Meta.UID)
-
-		if err := d.Set(common.MetaKey, common.FlattenMeta(resp.Provisioner.Meta)); err != nil {
-			return diag.FromErr(err)
+		p := &provisioner.VmwareTanzuManageV1alpha1ManagementclusterProvisionerListprovisionersResponse{
+			Provisioners: []*provisioner.VmwareTanzuManageV1alpha1ManagementclusterProvisionerProvisioner{
+				{
+					FullName: &provisioner.VmwareTanzuManageV1alpha1ManagementclusterProvisionerFullName{
+						ManagementClusterName: getResp.Provisioner.FullName.ManagementClusterName,
+						Name:                  getResp.Provisioner.FullName.Name,
+						OrgID:                 getResp.Provisioner.FullName.OrgID,
+					},
+					Meta: &objectmetamodel.VmwareTanzuCoreV1alpha1ObjectMeta{
+						Description:     getResp.Provisioner.Meta.Description,
+						Labels:          getResp.Provisioner.Meta.Labels,
+						UID:             getResp.Provisioner.Meta.UID,
+						ResourceVersion: getResp.Provisioner.Meta.ResourceVersion,
+					},
+				},
+			},
 		}
+
+		resp = p
 	}
+
+	err = tfModelDataConverter.FillTFSchema(resp, d)
+
+	if err != nil {
+		return diag.FromErr(errors.Wrapf(err, "Unable to populate tf schema"))
+	}
+
+	for _, prov := range resp.Provisioners {
+		id = append(id, prov.Meta.UID)
+	}
+
+	d.SetId(strings.Join(id, "_"))
 
 	return diags
 }
